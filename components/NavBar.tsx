b5e6f2d848c291c215454f45ad289fa641696668
jiwonmik <jiwonmik@gmail.com>
2023-04-12 16:54:01 +0900

    design: Navigation Bar UI

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -6,21 +7,40 @@ export default function NavBar() {
 
   return (
     <nav>
-      <Link legacyBehavior href={'/'}>
-        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
-      </Link>
-      <Link legacyBehavior href={'/about'}>
-        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
-      </Link>
+      <img src="/vercel.svg" />
+      <div>
+        <Link legacyBehavior href="/">
+          <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
+        </Link>
+        <Link legacyBehavior href="/about">
+          <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
+        </Link>
+      </div>
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          gap: 10px;
+          align-items: center;
+          flex-direction: column;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: yellow;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
