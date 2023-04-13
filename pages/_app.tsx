0a965a0269055acda5baeff1435926f4d8cb7217
jiwonmik <jiwonmik@gmail.com>
2023-04-13 21:32:11 +0900

    feat: fetch Movie data and render

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,53 @@
 import Seo from '@/components/Seo';
+import { useQuery } from 'react-query';
+
+const API_KEY = '119b01bec93b136b74081bf4b13e43cc';
+
+interface IGetMoviesProps {
+  page: number;
+  results: IMovieProps[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface IMovieProps {
+  poster_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
+
+function getMovies() {
+  return fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`).then((response) =>
+    response.json()
+  );
+}
 
 export default function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesProps>(['movies', 'popular'], getMovies);
+  console.log(isLoading);
   return (
     <div>
       <Seo title="Home"></Seo>
-      <h1>Hello</h1>
+      {isLoading ? (
+        <h4>Loading...</h4>
+      ) : (
+        data?.results.map((movie) => (
+          <div key={movie.id}>
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))
+      )}
     </div>
   );
 }
